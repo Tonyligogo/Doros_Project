2c12cf1b741debfa1d923ecc5232cb8e4d64a2b7
Tony Ligogo <tonyligogo@gmail.com>
2024-07-17 14:56:54 +0300

    Banner image modification

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,28 @@
 import Banner from '../assets/breezer10.jpg'
+import Banner2 from '../assets/banner2.jpg'
+import { useEffect, useState } from 'react';
 function Hero() {
+  const [newImage, setNewImage] = useState(false);
+  const [screensize, setScreensize] = useState();
+  useEffect(() => {
+    const handleResize = () => setScreensize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (screensize <= 900) {
+      setNewImage(true);
+    } else {
+      setNewImage(false);
+    }
+  }, [screensize, setNewImage]);
+
   return (
     <div id="#" className='my-16 scroll-mt-[64px]'>
-        <img src={Banner} alt="Banner image" />
+        <img src={!newImage ? Banner : Banner2} alt="Banner image" />
         <div className='grid grid-cols-1 gap-6 md:grid-cols-2 my-12'>
           <div className='w-full grid justify-items-center pl-7'>
           <p className="text-gray-700 sm:w-full md:w-3/4">Bacardi Breezer, Breezer for short, is a fruit-based alcoholic drink which comes in many flavours.</p>
